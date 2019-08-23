b2cd348d242ff9a2df24d752012dbe72cfc43018
Kentaro Fujiy <kentaro@manacadigital.com.br>
2019-08-23 03:16:01 +0000

    supergit

--- lib/generators/gallifrey_jointable_generator.rb
+++ lib/generators/gallifrey_jointable_generator.rb
@@ -34,7 +34,7 @@ class GallifreyJointableGenerator < Rails::Generators::Base
       "
  
       #migration_name = "create_join_table_for_#{sorted_model[0]}_and_#{sorted_model[1]}"
-      generate("model", "ContentPost content_id:integer post_id:integer")
+      generate("model", "CapaCover capa_id:integer cover_id:integer")
 
      # filename = Dir.glob("db/migrate/*#{migration_name}.rb")[0]
 
